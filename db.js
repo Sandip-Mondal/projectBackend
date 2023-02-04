9009e1bb47e235035a23b7459884d7dafd21d7d4
SANDIP <sandip24cse@gmail.com>
2023-02-04 14:19:16 +0530

    changes

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DATA_BASE)
-    .then(() => console.log('connect....')).catch((err) => console.log('not connect'));
+    .then().catch((err) => console.log('not connect'));
